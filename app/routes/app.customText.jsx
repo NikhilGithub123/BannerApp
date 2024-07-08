373de81b70d78d51c6c05155e080c5d34e35b1eb
Nikhil Gupta <nickrick028@gmail.com>
2024-07-08 13:52:04 +0530

    updated db

--- app/routes/app.customText.jsx
+++ app/routes/app.customText.jsx
@@ -1,20 +1,61 @@
 import {TextField} from '@shopify/polaris';
 import {useState, useCallback} from 'react';
+import { createOrUpdateBadge } from "../app.server"
+import {
+  useSubmit,
+} from "@remix-run/react";
+import {
+  PageActions,
+  Page,
+} from "@shopify/polaris";
+
+
+export async function action({ request, params }) {
+  console.log("inside action ", params);
+  const { session } = await authenticate.admin(request);
+  const { shop } = session;
+
+  const labelProductObjs = {
+    ...Object.fromEntries(await request.formData()),
+    shop,
+  };
+
+  console.log("object ", labelProductObjs)
+  const arrayToIterate = [labelProductObjs];
+  const result = createOrUpdateBanner(arrayToIterate);
+  return result;
+}
 
 function TextFieldExample() {
-  const [value, setValue] = useState('Nikhil Gupta');
+  const [value, setValue] = useState('Demo Banner Of Blackbytt');
 
   const handleChange = useCallback((newValue) => {
     setValue(newValue);
   }, []);
 
+  const submit = useSubmit();
+  function handleSave() {
+    const data = {
+      "bannerText": value,
+    };
+    submit(data, { method: "post" });
+  }
+
   return (
+    <Page>
     <TextField
       label="Store name"
       value={value}
       onChange={handleChange}
       autoComplete="off"
     />
+    <PageActions
+    primaryAction={{
+      content: "Save",
+      onAction: handleSave,
+    }}
+  />
+  </Page>
   );
 }
 
