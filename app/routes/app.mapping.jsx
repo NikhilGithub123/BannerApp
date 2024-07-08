c153abdddcecffcfb4a12b113b6f3080842c8709
Nikhil Gupta <nickrick028@gmail.com>
2024-07-08 15:52:08 +0530

    Dummy

--- app/routes/app.mapping.jsx
+++ app/routes/app.mapping.jsx
@@ -4,17 +4,17 @@ import {json} from '@remix-run/node'
 let banners;
 export async function loader({request}) {
     console.log("in mapping request");
-    const shopUrl = request.headers.get('Origin');
-    console.log("shopUrl ",shopUrl);
-    if(shopUrl)
-    {
+ //   const shopUrl = request.headers.get('Origin');
+ //   console.log("shopUrl ",shopUrl);
+ //   if(shopUrl)
+ //   {
         const shop = shopUrl.split('://')[1];
         console.log("storeName ", shop) 
         banners = await db.banner.findMany({
             where: { shop },
           });
         console.log("HELLO banners ", banners)
-    } 
+//    } 
     return json({
         data: banners 
     }, 
